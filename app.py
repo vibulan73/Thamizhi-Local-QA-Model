1b79020e13c1e5fa3cc9bc1af6176dbf2f9cd81d
vibulan73 <vibulan2000@gmail.com>
2025-07-15 16:50:38 +0530

    UI

--- app.py
+++ app.py
@@ -5,22 +5,22 @@ from model_handler import get_answer
 with open("static/style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 # App header
-st.markdown('<div class="header">🧠 தமிழ் கேள்வி - பதில் அமைப்பு</div>', unsafe_allow_html=True)
+st.markdown('<div class="header">Thamizhi - QA</div>', unsafe_allow_html=True)
 
 
 
 # Input form
-context = st.text_area("📘 உரை", height=150)
-question = st.text_input("❓ கேள்வி")
+context = st.text_area("Context", height=150)
+question = st.text_input("Question")
 
 
-if st.button("🔍 பதிலை காண்", type="primary"):
+if st.button("Extract Answer", type="primary"):
     if context.strip() and question.strip():
         answer = get_answer(context, question)
         st.markdown(f"""
             <div class="chat-bubble">
-                <strong>🤖 பதில்:</strong><br>{answer}
+                <strong>Answer:</strong><br>{answer}
             </div>
         """, unsafe_allow_html=True)
     else:
-        st.warning("தயவுசெய்து உரையும் கேள்வியும் உள்ளிடவும்.")
+        st.warning("Please input the context.")
